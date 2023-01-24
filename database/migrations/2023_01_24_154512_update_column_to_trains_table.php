e001c9011449c737bcd05e7e8ec29da2e213bd37
ManganiBiagio <biagiomangani@live.it>
2023-01-24 16:48:52 +0100

    migrations trains table

---
+++ database/migrations/2023_01_24_154512_update_column_to_trains_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string("agency");
+            $table->string("station_start");
+            $table->string("station_end");
+            $table->dateTime("start_at");
+            $table->dateTime("arrived_at");
+            $table->string("code");
+            $table->integer("carriage");
+            $table->boolean("in_time");
+            $table->boolean("deleted");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn("agency");
+            $table->dropColumn("station_start");
+            $table->dropColumn("station_end");
+            $table->dropColumn("start_at");
+            $table->dropColumn("arrived_at");
+            $table->dropColumn("code");
+            $table->dropColumn("carriage");
+            $table->dropColumn("in_time");
+            $table->dropColumn("deleted");
+        });
+    }
+};
