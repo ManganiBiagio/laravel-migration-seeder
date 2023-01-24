fd6300a2eb169c27abbcd8ecb299eb805f850b1c
ManganiBiagio <biagiomangani@live.it>
2023-01-24 17:27:01 +0100

    add filter for today in trainlist

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
+use DateTime;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
      public function index() {
-        return view("train");
+        $trainList=Train::whereDate("start_at",date("Y-m-d"))->get();
+        return view("train",compact("trainList"));
 
     }
 }
--- resources/views/train.blade.php
+++ resources/views/train.blade.php
@@ -1,7 +1,18 @@
 @extends("layouts.app")
 @section("content")
 <div class="container">
-    <h1>Treni</h1>
+    <h1>Treni in partenza oggi</h1>
+    <div class="row">
+        @foreach ($trainList as $train )
+        <div class="col">
+            Partenza:{{$train->station_start}}<br>
+            Arrivo:{{$train->station_end}}<br>
+            Data:{{$train->start_at}}
+            
+        </div>
+            
+        @endforeach
+    </div>
 </div>
     
 @endsection
