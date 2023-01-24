cb96e9d7a08ae4b2818c0c7e387fc8555598a687
ManganiBiagio <biagiomangani@live.it>
2023-01-24 18:02:24 +0100

    add date format

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    public function departureTimeToString(){
+        return $this->convertDateFromDb($this->start_at);
+        
+    }
+    public function arrivedTimeToString(){
+        return $this->convertDateFromDb($this->arrived_at);
+        
+    }
+    private function convertDateFromDb($toConvert){
+        return date("H:i d-m-Y ",strtotime($toConvert));
+
+    }
 }
--- resources/views/train.blade.php
+++ resources/views/train.blade.php
@@ -5,9 +5,19 @@
     <div class="row">
         @foreach ($trainList as $train )
         <div class="col">
-            Partenza:{{$train->station_start}}<br>
-            Arrivo:{{$train->station_end}}<br>
-            Data Partenza: {{$train->start_at}}
+            <div>
+                Partenza:{{$train->station_start}}
+            </div>
+            <div>
+                Arrivo:{{$train->station_end}}
+            </div>
+            <div>
+                Data Partenza: {{$train->departureTimeToString()}}
+            </div>
+            <div>
+                Data Arrivo: {{$train->arrivedTimeToString()}}
+            </div>
+
             
         </div>
             
